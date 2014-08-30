8fdf78e5b0b4fa6cd663ee5d423b58985ab873fd
hero <hero@asterisk-works.jp>
2014-08-30 15:59:31 +0900

    Setps API の実装
    最低限の実装はした。

--- app/controllers/api/v1/steps_controller.rb
+++ app/controllers/api/v1/steps_controller.rb
@@ -2,13 +2,24 @@ class Api::V1::StepsController < ApplicationController
 
   def update
 
-    id = params[:id]
+    device_id = params[:id]
     name = params[:name]
     count = params[:count]
 
-    @result = 'success'
-    @ranking = 2014
+    # StepCount の保存
+    sc = StepCount.find_by(device_id:device_id)
+    unless sc
+      sc = StepCount.new
+      sc.device_id=device_id
+    end
+    sc.name=name
+    sc.count=count
+    result = sc.save
 
+    # ランキングを求める
+    @ranking = StepCount.where("count > ?" , count).count + 1
+
+    @result = result ? 'success':'fail'
   end
 end
 
